f66b67cc50da60d1487ec7da97211aa5ae97a991
Basnayaka B.M.H.S <hiransanjeewaa@gmail.com>
2023-02-08 10:54:09 -0800

    adding change address page

---
+++ changeaddress.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <meta charset ="utf-8">
+        <meta http-equiv="x-UA-Compatible" content="IE-edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="Stylesheet" href="empty.css">
+        <style>
+            .inputfields{
+                  float: right;
+                  
+            }
+        </style>
+</head>
+<body>
+    
+    <div class="container-1">
+        <form action="changeaddress.php" style="text-align: left;">
+            Enter address line 1 <input type="text" are  name="adressline1" class="inputfields"><br><br>
+            Enter address line 2 <input type="text" name="adressline2" class="inputfields"><br><br>
+            District <input type="text" name="district" class="inputfields"><br><br>
+            Province <input type="text" name="province" class="inputfields"><br><br>
+            Country <input type="text" name="country" class="inputfields"><br><br>
+            Postal Code <input type="text" name="postalcode" class="inputfields"><br><br>
+    <br>
+    <button type="submit" >Change address</button>
+</form>
+    </div>
+    
+</body>
+</html>
